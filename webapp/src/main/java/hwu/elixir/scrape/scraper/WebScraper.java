0e27c1213e75be1923a6f5b8b077f06959f0523d
Kenneth McLeod <kcm1@hw.ac.uk>
2019-09-03 14:14:40 +0100

    using RDF4J to write quads ONLY puppeteer

--- webapp/src/main/java/hwu/elixir/scrape/scraper/WebScraper.java
+++ webapp/src/main/java/hwu/elixir/scrape/scraper/WebScraper.java
@@ -1,9 +1,13 @@
 package hwu.elixir.scrape.scraper;
 
+import java.io.ByteArrayOutputStream;
 import java.util.StringTokenizer;
 
 import org.apache.any23.source.DocumentSource;
 import org.apache.any23.source.StringDocumentSource;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.Rio;
 import org.json.simple.JSONArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,53 +23,44 @@ public class WebScraper extends ScraperCore {
 
 	private static final Logger logger = LoggerFactory.getLogger(System.class.getName());
 
-	public JSONArray scrape(String url)
-			throws HtmlExtractorServiceException, FourZeroFourException, JsonLDInspectionException, MissingContextException, MissingHTMLException {
+	public JSONArray scrape(String url) throws HtmlExtractorServiceException, FourZeroFourException,
+			JsonLDInspectionException, MissingContextException, MissingHTMLException {
 
 		if (url.endsWith("/") || url.endsWith("#"))
 			url = url.substring(0, url.length() - 1);
 
+		String html = GetHTMLFromNode.getHtml(url);		
 		
-		String html = getHtml(url);		
-		
-		try {
-			html = injectId(html, url);
-		} catch (MissingContextException | MissingHTMLException e) {			
-			logger.info("using puppeteer");
-			return scrapeWithPupeteer(url);	
-		}
-
-		DocumentSource source = new StringDocumentSource(html, url);
-
-		String n3 = getTriplesInN3(source);
-		if (n3 == null)
-			return scrapeWithPupeteer(url);
-			
-
-		return writeN3ToJSONArray(n3);
-	}
-
-	private JSONArray scrapeWithPupeteer(String url) throws HtmlExtractorServiceException, JsonLDInspectionException, MissingContextException, MissingHTMLException {
-		String html = GetHTMLFromNode.getHtml(url);
-
 		html = injectId(html, url);
 		
 		DocumentSource source = new StringDocumentSource(html, url);
-
 		String n3 = getTriplesInN3(source);
-		if (n3 == null)
-			return null;	
+		
+		if (n3 == null) {
+			logger.error("n3 is null");
+			throw new HtmlExtractorServiceException(url);
+		}
 
-		return writeN3ToJSONArray(n3);
+		Model model = processTriplesLeaveBlankNodes(n3);
+		if (model == null) {
+			logger.error("Model is null");
+			throw new HtmlExtractorServiceException(url);
+		}
+			
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		Rio.write(model, stream, RDFFormat.NTRIPLES);
+		String quads = new String(stream.toByteArray());
+
+		return writeN3ToJSONArray(quads);
 	}
 
-	
 	private JSONArray writeN3ToJSONArray(String n3) {
 		JSONArray array = new JSONArray();
 		StringTokenizer st = new StringTokenizer(n3, "\n");
-		while(st.hasMoreTokens()) {
+		while (st.hasMoreTokens()) {
 			array.add(st.nextToken());
 		}
 		return array;
 	}
+
 }
