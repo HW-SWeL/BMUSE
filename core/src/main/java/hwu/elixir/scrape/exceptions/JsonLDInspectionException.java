6bdba544a8beaea64d95a371b801dc86e37f7632
Kenneth McLeod <kcm1@hw.ac.uk>
2019-09-05 12:10:40 +0100

    Update JsonLDInspectionException.java

--- core/src/main/java/hwu/elixir/scrape/exceptions/JsonLDInspectionException.java
+++ core/src/main/java/hwu/elixir/scrape/exceptions/JsonLDInspectionException.java
@@ -8,7 +8,7 @@ public class JsonLDInspectionException extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	public JsonLDInspectionException(String url) {
-		super(url + " needs human inspection");
+	public JsonLDInspectionException(String msg) {
+		super(msg);
 	}
 }
