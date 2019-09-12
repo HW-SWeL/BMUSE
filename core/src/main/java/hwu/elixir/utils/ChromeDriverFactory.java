c9c229043a0fa27c38d889d34e9e7d82114155cf
Kenneth McLeod <kcm1@hw.ac.uk>
2019-09-12 15:27:57 +0100

    reading loc of chromeDriver from app.props

--- core/src/main/java/hwu/elixir/utils/ChromeDriverFactory.java
+++ core/src/main/java/hwu/elixir/utils/ChromeDriverFactory.java
@@ -1,5 +1,9 @@
 package hwu.elixir.utils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -7,12 +11,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ChromeDriverFactory {
+	
+	private static final String propertiesFile = "application.properties";
+	
 	private volatile static WebDriver driver;
 	private static ChromeOptions chromeOptions = new ChromeOptions();
 	private static Logger logger = LoggerFactory.getLogger(System.class.getName());
 	
 	static {
-		System.setProperty("webdriver.chrome.driver", "/Users/kcm/Applications/chromedriver");
+		ClassLoader classLoader = ChromeDriverFactory.class.getClassLoader();
+
+		InputStream is = classLoader.getResourceAsStream(propertiesFile);
+		if(is == null) {
+			logger.error("     Cannot find " + propertiesFile + " file");
+			throw new IllegalArgumentException(propertiesFile + "file is not found!");
+		}
+
+		Properties prop = new Properties();
+
+		try {
+			prop.load(is);
+		} catch (IOException e) {
+			logger.error("     Cannot load application.properties", e);
+			System.exit(0);
+		}
+
+		String chromiumDriverLoc  = prop.getProperty("chromiumDriverLocation").trim();
+		logger.info("     location of chromiun driver: " + chromiumDriverLoc);
+		System.setProperty("webdriver.chrome.driver", chromiumDriverLoc);
 		chromeOptions.addArguments("--headless");
 	}
 	
