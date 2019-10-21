3123c249254b44f7df4c9178b49283fcd715d136
Kenneth McLeod <kcm1@hw.ac.uk>
2019-10-21 13:50:37 +0100

    updating tests

--- core/src/test/java/hwu/elixir/scrape/scraper/ScraperCoreTest.java
+++ core/src/test/java/hwu/elixir/scrape/scraper/ScraperCoreTest.java
@@ -28,40 +28,45 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mockito;
+
 import hwu.elixir.scrape.exceptions.FourZeroFourException;
 import hwu.elixir.scrape.exceptions.JsonLDInspectionException;
 import hwu.elixir.scrape.exceptions.MissingHTMLException;
 import hwu.elixir.scrape.exceptions.SeleniumException;
 import hwu.elixir.utils.CompareNQ;
 
-public class FileScraperTest {
-
-	private static FileScraper fileScraper;
+public class ScraperCoreTest {
+	
+	private static ScraperCore scraperCore;
 	private static File testHtml;
 
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		fileScraper = new FileScraper();
+	public static void setUpBeforeClass() throws Exception {			
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		fileScraper.shutdown();
+		scraperCore.shutdown();
 	}
 
 	@Before
-	public void setUp() throws Exception {
-//		simpleCore = Mockito.mock(ScraperCore.class, Mockito.CALLS_REAL_METHODS);
+	public void setUp() throws Exception {		
+		scraperCore = Mockito.mock(ScraperCore.class, Mockito.CALLS_REAL_METHODS);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
-
+	
+	/*
+	 * 
+	 */
+	
 	@Test
 	public void test_fixAny23WeirdIssues() {
 		String toFix = "<div>license</div> file format file Format \tFile Format fileFormat<p></p>\n additionalType";
-		String fixed = fileScraper.fixAny23WeirdIssues(toFix);
+		String fixed = scraperCore.fixAny23WeirdIssues(toFix);
 		assertEquals("<div>licensE</div> file format file Format \tFile Format FileFormat<p></p>\n addType", fixed);
 	}
 
@@ -70,7 +75,7 @@ public class FileScraperTest {
 		String nGraph = "https://bioschemas.org/crawl/v1/0";
 		IRI sourceIRI = SimpleValueFactory.getInstance().createIRI("https://www.macs.hw.ac.uk");
 
-		IRI randomIRI = fileScraper.iriGenerator(nGraph, sourceIRI);
+		IRI randomIRI = scraperCore.iriGenerator(nGraph, sourceIRI);
 
 		String uri = randomIRI.stringValue();
 		assertTrue(uri.startsWith("https://bioschemas.org/crawl/v1/0/www.macs.hw.ac.uk/"));
@@ -85,7 +90,7 @@ public class FileScraperTest {
 		nGraph = "https://bioschemas.org/crawl/v1/0";
 		sourceIRI = SimpleValueFactory.getInstance().createIRI("http://www.macs.hw.ac.uk#");
 
-		randomIRI = fileScraper.iriGenerator(nGraph, sourceIRI);
+		randomIRI = scraperCore.iriGenerator(nGraph, sourceIRI);
 
 		uri = randomIRI.stringValue();
 		assertTrue(uri.startsWith("https://bioschemas.org/crawl/v1/0/www.macs.hw.ac.uk#"));
@@ -103,50 +108,50 @@ public class FileScraperTest {
 	public void test_fixPredicate() {
 		IRI sourceIRI = SimpleValueFactory.getInstance().createIRI("https://www.macs.hw.ac.uk/");
 		IRI expectedIRI = SimpleValueFactory.getInstance().createIRI("https://www.macs.hw.ac.uk");
-		IRI fixedIRI = fileScraper.fixPredicate(sourceIRI);
+		IRI fixedIRI = scraperCore.fixPredicate(sourceIRI);
 		assertEquals(expectedIRI, fixedIRI);
 
 		sourceIRI = SimpleValueFactory.getInstance().createIRI("https://schema.org/licensE");
 		expectedIRI = SimpleValueFactory.getInstance().createIRI("https://schema.org/license");
-		fixedIRI = fileScraper.fixPredicate(sourceIRI);
+		fixedIRI = scraperCore.fixPredicate(sourceIRI);
 		assertEquals(expectedIRI, fixedIRI);
 
 		sourceIRI = SimpleValueFactory.getInstance().createIRI("https://bioschemas.org/addType");
 		expectedIRI = SimpleValueFactory.getInstance().createIRI("https://bioschemas.org/additionalType");
-		fixedIRI = fileScraper.fixPredicate(sourceIRI);
+		fixedIRI = scraperCore.fixPredicate(sourceIRI);
 		assertEquals(expectedIRI, fixedIRI);
 
 		sourceIRI = SimpleValueFactory.getInstance().createIRI("http://bioschemas.org/addType");
 		expectedIRI = SimpleValueFactory.getInstance().createIRI("https://bioschemas.org/additionalType");
-		fixedIRI = fileScraper.fixPredicate(sourceIRI);
+		fixedIRI = scraperCore.fixPredicate(sourceIRI);
 		assertEquals(expectedIRI, fixedIRI);
 
 		sourceIRI = SimpleValueFactory.getInstance().createIRI("http://schema.org/licensE");
 		expectedIRI = SimpleValueFactory.getInstance().createIRI("https://schema.org/license");
-		fixedIRI = fileScraper.fixPredicate(sourceIRI);
+		fixedIRI = scraperCore.fixPredicate(sourceIRI);
 		assertEquals(expectedIRI, fixedIRI);
 	}
 
 	@Test(expected = FourZeroFourException.class)
 	public void test_getHtml_404() throws FourZeroFourException {
-		fileScraper.getHtml("https://www.macs.hw.ac.uk/~kcm/bannananan.html");
+		scraperCore.getHtml("https://www.macs.hw.ac.uk/~kcm/bannananan.html");
 	}
 
 	@Test
 	public void test_getHtml() throws FourZeroFourException {
-		String html = fileScraper.getHtml("https://www.macs.hw.ac.uk");
+		String html = scraperCore.getHtml("https://www.macs.hw.ac.uk");
 		assert (html.contains("CollegeOrUniversity") && html.contains("BreadcrumbList"));
 	}
 
 	@Test
 	public void test_getHtmlViaSelenium() throws FourZeroFourException, SeleniumException {
-		String html = fileScraper.getHtmlViaSelenium("https://www.macs.hw.ac.uk");
+		String html = scraperCore.getHtmlViaSelenium("https://www.macs.hw.ac.uk");
 		assert (html.contains("CollegeOrUniversity") && html.contains("BreadcrumbList"));
 	}
 
 	@Test
 	public void test_getOnlyJSONLD() throws FourZeroFourException, SeleniumException {
-		String[] allJsonLD = fileScraper.getOnlyJSONLD("http://www.macs.hw.ac.uk");
+		String[] allJsonLD = scraperCore.getOnlyJSONLD("http://www.macs.hw.ac.uk");
 		assertTrue(allJsonLD.length == 3);
 		for (String json : allJsonLD) {
 			assertTrue(json.startsWith("{") && json.endsWith("}"));
@@ -169,7 +174,7 @@ public class FileScraperTest {
 				html = html.trim();
 			}
 
-			String outHtml = fileScraper.injectId(html, "http://test.com");
+			String outHtml = scraperCore.injectId(html, "http://test.com");
 			assertEquals(html, outHtml);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -194,13 +199,13 @@ public class FileScraperTest {
 			}
 
 			// no trailing /
-			String outHtml = fileScraper.injectId(html, "https://myId.com");
+			String outHtml = scraperCore.injectId(html, "https://myId.com");
 			assertTrue(outHtml.contains("\"@id\":\"https://myId.com\","));
 
 			// got a trailing /
 			String html2 = html.replaceFirst("\"@context\": \"http://schema.org\"",
 					"\"@context\": \"http://schema.org/\"");
-			outHtml = fileScraper.injectId(html2, "https://myId.com");
+			outHtml = scraperCore.injectId(html2, "https://myId.com");
 			assertTrue(outHtml.contains("\"@id\":\"https://myId.com\","));
 
 			// context at end of JSON-LD
@@ -215,7 +220,7 @@ public class FileScraperTest {
 				}
 				html = html.trim();
 			}
-			outHtml = fileScraper.injectId(html, "https://myId.com");
+			outHtml = scraperCore.injectId(html, "https://myId.com");
 			assertTrue(outHtml.contains("\"@id\":\"https://myId.com\""));
 
 		} catch (Exception e) {
@@ -238,7 +243,7 @@ public class FileScraperTest {
 				}
 				html = html.trim();
 			}
-			String outHtml = fileScraper.injectId(html, "https://myId.com");
+			String outHtml = scraperCore.injectId(html, "https://myId.com");
 			assertTrue(outHtml.contains("\"@id\":\"https://myId.com\","));
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -260,7 +265,7 @@ public class FileScraperTest {
 				}
 				html = html.trim();
 			}
-			String outHtml = fileScraper.injectId(html, "https://myId.com");
+			String outHtml = scraperCore.injectId(html, "https://myId.com");
 			System.out.println(outHtml);
 			assertTrue(outHtml.contains("\"@id\":\"https://myId.com\","));
 		} catch (Exception e) {
@@ -268,7 +273,8 @@ public class FileScraperTest {
 			fail();
 		}
 	}
-
+	
+	@Test
 	public void test_injectId_MissingContext() throws MissingHTMLException, JsonLDInspectionException {
 		String resourceName = "testHtml/contextAtEnd.html";
 		ClassLoader classLoader = getClass().getClassLoader();
@@ -283,9 +289,10 @@ public class FileScraperTest {
 
 			html = html.replaceFirst("@context", "@apple");
 
-			html = fileScraper.injectId(html, "https://myId.com");
+			html = scraperCore.injectId(html, "https://myId.com");
 
-			assertTrue(html.contains("\"@context\":\"https://myId.com\""));
+			assertTrue(html.contains("\"@context\":\"https://schema.org\""));
+			assertTrue(html.contains("\"@id\":\"https://myId.com\""));
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -298,12 +305,12 @@ public class FileScraperTest {
 
 	@Test(expected = MissingHTMLException.class)
 	public void test_injectId_MHException() throws MissingHTMLException, JsonLDInspectionException {
-		fileScraper.injectId(null, "https://myId.com");
+		scraperCore.injectId(null, "https://myId.com");
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void test_injectId_nullURL() throws MissingHTMLException, JsonLDInspectionException {
-		fileScraper.injectId("", null);
+		scraperCore.injectId("", null);
 	}
 
 	@Test
@@ -319,7 +326,7 @@ public class FileScraperTest {
 			}
 			html = html.trim();
 
-			String outHtml = fileScraper.injectId(html, "https://myId.com");
+			String outHtml = scraperCore.injectId(html, "https://myId.com");
 			assertEquals(outHtml, html);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -333,7 +340,7 @@ public class FileScraperTest {
 	@Test
 	public void test_fixObject_BNode() {
 		BNode bNode = SimpleValueFactory.getInstance().createBNode("a bNode");
-		Value outNode = fileScraper.fixObject(bNode);
+		Value outNode = scraperCore.fixObject(bNode);
 
 		assertEquals(outNode, bNode);
 	}
@@ -341,33 +348,33 @@ public class FileScraperTest {
 	@Test
 	public void test_fixObject() {
 		IRI inIRI = SimpleValueFactory.getInstance().createIRI("http://bioschemas.org");
-		Value outIRI = fileScraper.fixObject(inIRI);
+		Value outIRI = scraperCore.fixObject(inIRI);
 		assertEquals("https://bioschemas.org", outIRI.stringValue());
 
 		inIRI = SimpleValueFactory.getInstance().createIRI("http://schema.org/DataSet");
-		outIRI = fileScraper.fixObject(inIRI);
+		outIRI = scraperCore.fixObject(inIRI);
 		assertEquals("https://schema.org/Dataset", outIRI.stringValue());
 
 		inIRI = SimpleValueFactory.getInstance().createIRI("http://schema.org/dataSet");
-		outIRI = fileScraper.fixObject(inIRI);
+		outIRI = scraperCore.fixObject(inIRI);
 		assertEquals("https://schema.org/Dataset", outIRI.stringValue());
 
 		inIRI = SimpleValueFactory.getInstance().createIRI("http://schema.org/dataset/");
-		outIRI = fileScraper.fixObject(inIRI);
+		outIRI = scraperCore.fixObject(inIRI);
 		assertEquals("https://schema.org/Dataset", outIRI.stringValue());
 
 		inIRI = SimpleValueFactory.getInstance()
 				.createIRI("http://schema.org/dataset/www.bioschemas.org/licensE/FileFormat/addType");
-		outIRI = fileScraper.fixObject(inIRI);
+		outIRI = scraperCore.fixObject(inIRI);
 		assertEquals("https://www.bioschemas.org/license/fileFormat/additionalType", outIRI.stringValue());
 
 		inIRI = SimpleValueFactory.getInstance().createIRI("https://schema.org/dataset/www.bioschemas.org/licensE");
-		outIRI = fileScraper.fixObject(inIRI);
+		outIRI = scraperCore.fixObject(inIRI);
 		assertEquals("https://www.bioschemas.org/license", outIRI.stringValue());
 
 		Literal inLiteral = SimpleValueFactory.getInstance()
 				.createLiteral("https://schema.org/dataset/www.bioschemas.org/licensE/FileFormat/addType");
-		Value outLiteral = fileScraper.fixObject(inLiteral);
+		Value outLiteral = scraperCore.fixObject(inLiteral);
 		assertEquals("https://schema.org/dataset/www.bioschemas.org/license/fileFormat/additionalType",
 				outLiteral.stringValue());
 	}
@@ -376,21 +383,21 @@ public class FileScraperTest {
 	public void test_fixASingleContext() throws JsonLDInspectionException {
 		JSONObject obj = new JSONObject();
 		obj.put("key1", "value1");
-		String fixedJSON = fileScraper.fixASingleJsonLdBlock(obj.toJSONString(), "http://www.myId.org");
+		String fixedJSON = scraperCore.fixASingleJsonLdBlock(obj.toJSONString(), "http://www.myId.org");
 		assertTrue(fixedJSON.contains("\"@context\":\"https://schema.org\""));
 		assertTrue(fixedJSON.contains("\"@id\":\"http://www.myId.org\""));
 
 		obj = new JSONObject();
 		obj.put("key1", "value1");
 		obj.put("@context", "https://schema.org");
-		fixedJSON = fileScraper.fixASingleJsonLdBlock(obj.toJSONString(), "http://www.myId.org");
+		fixedJSON = scraperCore.fixASingleJsonLdBlock(obj.toJSONString(), "http://www.myId.org");
 		assertTrue(fixedJSON.contains("\"@context\":\"https://schema.org\""));
 		assertTrue(fixedJSON.contains("\"@id\":\"http://www.myId.org\""));
 
 		obj = new JSONObject();
 		obj.put("key1", "value1");
 		obj.put("@context", "http://www.macs.hw.ac.uk");
-		fixedJSON = fileScraper.fixASingleJsonLdBlock(obj.toJSONString(), "http://www.myId.org");
+		fixedJSON = scraperCore.fixASingleJsonLdBlock(obj.toJSONString(), "http://www.myId.org");
 		assertTrue(fixedJSON.contains("\"@context\":\"https://schema.org\""));
 		assertTrue(fixedJSON.contains("\"@id\":\"http://www.myId.org\""));
 
@@ -398,7 +405,7 @@ public class FileScraperTest {
 		obj.put("key1", "value1");
 		obj.put("@context", "https://schema.org");
 		obj.put("@id", "http://www.myId.org");
-		fixedJSON = fileScraper.fixASingleJsonLdBlock(obj.toJSONString(), "http://www.myId.org");
+		fixedJSON = scraperCore.fixASingleJsonLdBlock(obj.toJSONString(), "http://www.myId.org");
 		assertTrue(fixedJSON.contains("\"@context\":\"https://schema.org\""));
 		assertTrue(fixedJSON.contains("\"@id\":\"http://www.myId.org\""));
 	}
@@ -422,7 +429,7 @@ public class FileScraperTest {
 			e.printStackTrace();
 			fail();
 		}
-		String[] allJsonMarkup = fileScraper.getJSONLDMarkup(html);
+		String[] allJsonMarkup = scraperCore.getJSONLDMarkup(html);
 		assertTrue(allJsonMarkup.length == 4);
 
 		for (String json : allJsonMarkup) {
@@ -459,19 +466,19 @@ public class FileScraperTest {
 			fail();
 		}
 
-		String[] allMarkup = fileScraper.getJSONLDMarkup(html);
+		String[] allMarkup = scraperCore.getJSONLDMarkup(html);
 		int i = 10;
 		for (String oldMarkup : allMarkup) {
 			JSONObject obj = new JSONObject();
 			obj.put("newBlock", Integer.toString(i++));
-			html = fileScraper.swapJsonLdMarkup(html, oldMarkup, obj.toJSONString());
+			html = scraperCore.swapJsonLdMarkup(html, oldMarkup, obj.toJSONString());
 		}
 
 		for (String oldMarkup : allMarkup) {
 			assertFalse(html.contains(oldMarkup));
 		}
 
-		String[] newAllMarkup = fileScraper.getJSONLDMarkup(html);
+		String[] newAllMarkup = scraperCore.getJSONLDMarkup(html);
 		for (String newMarkup : newAllMarkup) {
 			assertTrue(html.contains(newMarkup));
 		}
@@ -492,7 +499,7 @@ public class FileScraperTest {
 				}
 				html = html.trim();
 			}
-			html = fileScraper.injectId(html, "https://hamap.expasy.org");
+			html = scraperCore.injectId(html, "https://hamap.expasy.org");
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -500,8 +507,8 @@ public class FileScraperTest {
 		}
 		DocumentSource source = new StringDocumentSource(html, "https://hamap.expasy.org");
 		IRI sourceIRI = SimpleValueFactory.getInstance().createIRI(source.getDocumentIRI());
-		String n3 = fileScraper.getTriplesInNTriples(source);
-		Model liveModel = fileScraper.processTriples(n3, sourceIRI, 100000L);
+		String n3 = scraperCore.getTriplesInNTriples(source);
+		Model liveModel = scraperCore.processTriples(n3, sourceIRI, 100000L);
 
 		File liveQuads = new File("test_live_hamap.nq");
 
@@ -544,7 +551,7 @@ public class FileScraperTest {
 				}
 				html = html.trim();
 			}
-			html = fileScraper.injectId(html, "http://www.macs.hw.ac.uk/shouldNotBeInjected");
+			html = scraperCore.injectId(html, "http://www.macs.hw.ac.uk/shouldNotBeInjected");
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -552,8 +559,8 @@ public class FileScraperTest {
 		}
 		DocumentSource source = new StringDocumentSource(html, "https://www.ebi.ac.uk/biosamples/samples/SAMEA4999347");
 		IRI sourceIRI = SimpleValueFactory.getInstance().createIRI(source.getDocumentIRI());
-		String n3 = fileScraper.getTriplesInNTriples(source);
-		Model liveModel = fileScraper.processTriples(n3, sourceIRI, 100000L);
+		String n3 = scraperCore.getTriplesInNTriples(source);
+		Model liveModel = scraperCore.processTriples(n3, sourceIRI, 100000L);
 
 		File liveQuads = new File("test_live_biosamples.nq");
 
@@ -598,7 +605,7 @@ public class FileScraperTest {
 				}
 				html = html.trim();
 			}
-			html = fileScraper.injectId(html, "https://www.ebi.ac.uk/chembl/compound_report_card/CHEMBL59/");
+			html = scraperCore.injectId(html, "https://www.ebi.ac.uk/chembl/compound_report_card/CHEMBL59/");
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -607,8 +614,8 @@ public class FileScraperTest {
 		DocumentSource source = new StringDocumentSource(html,
 				"https://www.ebi.ac.uk/chembl/compound_report_card/CHEMBL59/");
 		IRI sourceIRI = SimpleValueFactory.getInstance().createIRI(source.getDocumentIRI());
-		String n3 = fileScraper.getTriplesInNTriples(source);
-		Model liveModel = fileScraper.processTriples(n3, sourceIRI, 100000L);
+		String n3 = scraperCore.getTriplesInNTriples(source);
+		Model liveModel = scraperCore.processTriples(n3, sourceIRI, 100000L);
 
 		File liveQuads = new File("test_live_chembl.nq");
 
@@ -635,5 +642,16 @@ public class FileScraperTest {
 		}
 
 		liveQuads.delete();
+	}	
+	
+	
+	@Test
+	public void wrapHTMLExtraction_basic() throws FourZeroFourException, SeleniumException {
+		String url = "https://www.macs.hw.ac.uk";
+		String html1 = scraperCore.getHtmlViaSelenium(url);
+		String html2 = scraperCore.wrapHTMLExtraction(url);
+		
+		assertEquals(html2, html1);
 	}
+
 }
