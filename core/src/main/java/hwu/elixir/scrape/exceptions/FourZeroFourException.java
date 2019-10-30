f483468ad6d32f6e2fb2578cf39ab504f2ff828c
Kenneth McLeod <kcm1@hw.ac.uk>
2019-10-30 13:19:35 +0000

    extending Javadoc

--- core/src/main/java/hwu/elixir/scrape/exceptions/FourZeroFourException.java
+++ core/src/main/java/hwu/elixir/scrape/exceptions/FourZeroFourException.java
@@ -5,6 +5,8 @@ public class FourZeroFourException extends Exception {
 	/**
 	 * 404 returned by server when trying to scrape a URL.
 	 * 
+	 * As Selenium hides the HTML codes, this is not much use.
+	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	
