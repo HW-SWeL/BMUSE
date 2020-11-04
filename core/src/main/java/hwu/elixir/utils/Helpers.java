22157680c9467b9b00c00ad017b1caecdfee7eb7
petrospaps <p.papadopoulos@hw.ac.uk>
2020-11-04 12:32:40 +0000

    gz decompression method for sitemaps that use compression.

--- core/src/main/java/hwu/elixir/utils/Helpers.java
+++ core/src/main/java/hwu/elixir/utils/Helpers.java
@@ -1,15 +1,62 @@
 package hwu.elixir.utils;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.zip.GZIPInputStream;
 
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 public class Helpers {
+
+	/**
+	 * Method that takes a gz file and returns the decompressed contents of the file
+	 * @param sourceFile
+	 * @return decomressedFile
+	 */
+	public static String gzipFileDecompression(String sourceFile) {
+
+
+		String decompressedFile = sourceFile.substring(0, sourceFile.length() - 3);
+
+		//String content = new String(new GZIP().decompresGzipToBytes(FileUtils.readFileToByteArray(fileName)), "UTF-8");
+
+		try{
+			// Create a file input stream to read the source file.
+			// Create a gzip input stream to decompress the source
+			// file defined by the file input stream.
+			GZIPInputStream gzis = new GZIPInputStream(new FileInputStream(sourceFile));
+
+			// Create file output stream where the decompress result
+			// will be stored.
+			FileOutputStream fos = new FileOutputStream(decompressedFile);
+
+			// Create a buffer and temporary variable used during the
+			// file decompress process.
+			byte[] buffer = new byte[1024];
+			int length;
+
+			// Read from the compressed source file and write the
+			// decompress file.
+			while ((length = gzis.read(buffer)) > 0) {
+				fos.write(buffer, 0, length);
+			}
+
+			//gzis.close();
+			//fos.close();
+
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+
+		return decompressedFile;
+	}
 
 	/**
 	 * Generates a date in the format yyyy-MM-dd.
