d73bd7fc03ad324ca8a64a35e402907fdc2dcaa6
petrospaps <p.papadopoulos@hw.ac.uk>
2021-04-26 18:23:00 +0100

    moved the webscraper to the filtered scraper that does a better job of cleaning the markup, removed previous workaround for adding schema direct link to markup

--- webapp/src/main/java/hwu/elixir/scrape/scraper/WebScraper.java
+++ webapp/src/main/java/hwu/elixir/scrape/scraper/WebScraper.java
@@ -2,9 +2,12 @@ package hwu.elixir.scrape.scraper;
 
 import java.io.ByteArrayOutputStream;
 
+import hwu.elixir.scrape.exceptions.*;
 import org.apache.any23.source.DocumentSource;
 import org.apache.any23.source.StringDocumentSource;
+import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.Rio;
 import org.json.simple.JSONArray;
@@ -12,76 +15,71 @@ import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import hwu.elixir.scrape.exceptions.FourZeroFourException;
-import hwu.elixir.scrape.exceptions.JsonLDInspectionException;
-import hwu.elixir.scrape.exceptions.MissingHTMLException;
-import hwu.elixir.scrape.exceptions.SeleniumException;
-
 /** 
- * Scrapes a given site and returns the raw (unfiltered/unprocessed) n-triples wrapped in a {@link JSONArray}
+ * Scrapes a given site and returns n-triples wrapped in a {@link JSONArray}
  * 
  *
  */
-public class WebScraper extends ScraperUnFilteredCore {
+public class WebScraper extends ScraperFilteredCore {
 
 	private static final Logger logger = LoggerFactory.getLogger(WebScraper.class.getName());
-	private String appVer = properties.getScraperVersion();
-
-
 
 	public JSONArray scrape(String url, ScraperOutput scraperOutputType) throws FourZeroFourException,
-			JsonLDInspectionException, MissingHTMLException, SeleniumException, Exception {				
+			JsonLDInspectionException, MissingHTMLException, SeleniumException, Exception {
 
-		logger.info("Scraper Version: " + appVer);
-		//remove http/s://schema.org from source and replace with the direct link
-		String tempSource = getHtmlViaSelenium(fixURL(url));
-		tempSource = removeSchemaORG(tempSource);
-		DocumentSource source = new StringDocumentSource(tempSource, url);
-		String triples = getTriplesInNTriples(source);
-		//logger.info("triples from source: " + triples.toString());
-		
-		if (triples == null) {
-			logger.error("n3 is null");
-			throw new Exception("Cannot obtain triples from "+url);
+		// parse the html with selenium, dynamic parsing
+		String html = wrapHTMLExtraction(url);
+
+		if (html == null || html.contentEquals("")) {
+			logger.info("html is null or content equals to cs");
 		}
 
-		Model model = processTriplesLeaveBlankNodes(triples);
-		if (model == null) {
+		try {
+			html = injectId(html, url);
+		} catch (MissingHTMLException e) {
+			logger.error(e.toString());
+		}
+
+		// write html markup blocks in ntriples form
+		DocumentSource source = new StringDocumentSource(html, url);
+		IRI sourceIRI = SimpleValueFactory.getInstance().createIRI(source.getDocumentIRI());
+		String n3 = getTriplesInNTriples(source);
+		
+		if (n3 == null) {
+			throw new MissingMarkupException(url);
+		}
+
+		Model updatedModel = null;
+
+		try {
+			updatedModel = processTriples(n3, sourceIRI, 0L);
+		} catch (NTriplesParsingException e1) {
+			logger.error("Failed to process triples into model; the NTriples generated from the URL (" + url
+					+ ") could not be parsed into a model.");
+		}
+
+		if (updatedModel == null) {
 			logger.error("Model is null");
 			throw new Exception("Cannot build model for " + url);
 		}
 		
 		JSONArray outputArray = new JSONArray();
 		ByteArrayOutputStream stream = new ByteArrayOutputStream();
-		if(scraperOutputType.equals(ScraperOutput.JSONLD)) {
-			
-			Rio.write(model, stream, RDFFormat.JSONLD);
-			String quads = new String(stream.toByteArray());
 
-			//logger.info("JSONLD quads from Rio: " + quads.toString());
-		
+		// Check the required output and write the file accordingly, supported JSONLD and TURTLE
+		if(scraperOutputType.equals(ScraperOutput.JSONLD)) {
+			Rio.write(updatedModel, stream, RDFFormat.JSONLD);
+			String quads = new String(stream.toByteArray());
 			JSONParser parser = new JSONParser();
 			outputArray = (JSONArray) parser.parse(quads);
 		} else if(scraperOutputType.equals(ScraperOutput.TURTLE)) {
-			Rio.write(model, stream, RDFFormat.TURTLE);
+			Rio.write(updatedModel, stream, RDFFormat.TURTLE);
 			String quads = new String(stream.toByteArray());
-			//logger.info("TURTLE quads from Rio: " + quads.toString());
 			outputArray.add(quads);
+		} else {
+			logger.error("unsupported output format");
 		}
 		
 		return outputArray;
 	}
-
-	/**
-	 * method that removes all http/s://schema.org instances and replaces them with the direct link
-	 * @param source
-	 * @return
-	 */
-	private String removeSchemaORG (String source) {
-		String alteredSource = "";
-		String tempSchemaContext = properties.getSchemaContext();
-		alteredSource = source.replaceAll("htt(p|ps):\\/\\/schema\\.org", tempSchemaContext);
-		return alteredSource;
-	}
-
 }
