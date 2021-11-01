37d9feadfbb31bb840de85e96795979b5341a0ad
Petros Papadopoulos <petros.papadopoulos@gmail.com>
2021-11-01 19:03:50 +0000

    Fixed issue with wrong URL when scraping sitemaps and there is an error.

--- core/src/main/java/hwu/elixir/scrape/scraper/examples/FileScraper.java
+++ core/src/main/java/hwu/elixir/scrape/scraper/examples/FileScraper.java
@@ -227,16 +227,16 @@ public class FileScraper extends ScraperFilteredCore {
 						result = scrape(sitemapURL.text(), properties.getOutputFolder(), null, contextCounter++, dynamicScrape);
 					} catch (FourZeroFourException e) {
 						logger.error(url + "returned a 404.");
-						unscrapedURLsToFile(outputFolder, null, url, contextCounter);
+						unscrapedURLsToFile(outputFolder, null, sitemapURL.text(), contextCounter - 1L);
 					} catch (JsonLDInspectionException e) {
 						logger.error("The JSON-LD could not be parsed for " + url);
-						unscrapedURLsToFile(outputFolder, null, url, contextCounter);
+						unscrapedURLsToFile(outputFolder, null, sitemapURL.text(), contextCounter - 1L);
 					} catch (CannotWriteException e) {
-						logger.error("Problem writing file for " + url + " to the " + properties.getOutputFolder() + " directory.");
-						unscrapedURLsToFile(outputFolder, null, url, contextCounter);
+						logger.error("Problem writing file for " + sitemapURL.text() + " to the " + properties.getOutputFolder() + " directory.");
+						unscrapedURLsToFile(outputFolder, null, sitemapURL.text(), contextCounter - 1L);
 					} catch (MissingMarkupException e) {
-						logger.error("Problem obtaining markup from " + url + ".");
-						unscrapedURLsToFile(outputFolder, null, url, contextCounter);
+						logger.error("Problem obtaining markup from " + sitemapURL.text() + ".");
+						unscrapedURLsToFile(outputFolder, null, sitemapURL.text(), contextCounter - 1L);
 					}
 					displayResult(sitemapURL.text(), result, properties.getOutputFolder(), contextCounter - 1L);
 					sitemapCount++;
